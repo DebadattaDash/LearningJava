25e17fdb29cc26847b6f35f44fb97a82ed138417
Debadatta Dash <dash.debad@gmail.com>
2025-06-05 10:02:05 +0530

    Add Lab003 added

--- src/ex_01_Java_Basic/Lab003_NH.java
+++ src/ex_01_Java_Basic/Lab003_NH.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic;
 
 public class Lab003_NH {
     public static void main(String[] args) {
-        System.out.println("new human");
+        System.out.println("new human1");
     }
 }
